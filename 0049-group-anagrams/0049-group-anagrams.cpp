adf887eb521d05e3ee8b9ee72d3bea6950469693
Badal Pipalde <badalpipalde3@gmail.com>
2026-08-31 15:41:28 +0530

    Time: 11 ms (91.11%), Space: 26.2 MB (31.1%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string, vector<string>>m;
+        for(int i =0; i<strs.size(); i++){
+            string c = strs[i];
+            sort(c.begin(), c.end());
+            m[c].push_back(strs[i]);
+        }
+        for(auto it : m){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
